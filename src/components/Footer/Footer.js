d96d95343ad615fe0e1577ae0d02e9b499160442
Tanvir-51 <tanvir19ahmed@gmail.com>
2021-10-04 14:39:38 +0600

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="Footer-container">
+      <h2 className="fw-bold mb-4">
+        Start Your Journey With Programming-School
+      </h2>
+      <h5>This is All You need To Start Your Programming Career</h5>
+      <p>Best Programming Courses at a Cost-Effective Price</p>
+      <button className="btn btn-success">Purchase Now at $200</button>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="fw-bold">Programming-School</h1>
+    </div>
+  );
+};
+
+export default Header;
